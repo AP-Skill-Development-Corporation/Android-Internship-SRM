45a95d59c1563122975de4f3f8d00a8fcb588c82
unknown <g.v.saisankar@gmail.com>
2020-06-25 15:15:50 +0530

    Count App Java Code is Completed

--- CountApp/app/src/main/java/com/example/countapp/MainActivity.java
+++ CountApp/app/src/main/java/com/example/countapp/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.countapp;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1;
+    TextView tv;
+    int a=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1 =findViewById(R.id.countInc);
+        tv = findViewById(R.id.textcount);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Action
+                a++;
+                tv.setText(""+a);
+
+            }
+        });
+
+    }
+
+
+    public void display(View view) {
+        //Action
+        Toast.makeText(this,"Welcome to SRM",Toast.LENGTH_SHORT).show();
     }
 }
