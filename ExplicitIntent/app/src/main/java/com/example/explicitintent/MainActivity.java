5ac074395d9e9f306206eccf9521019b8d0b77fe
unknown <g.v.saisankar@gmail.com>
2020-06-27 12:20:13 +0530

    Data Parsing

--- ExplicitIntent/app/src/main/java/com/example/explicitintent/MainActivity.java
+++ ExplicitIntent/app/src/main/java/com/example/explicitintent/MainActivity.java
@@ -5,17 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        et = findViewById(R.id.username);
     }
 
     public void submit(View view) {
-        Intent i = new Intent(MainActivity.this,SecondActivity.class);
-        startActivity(i);
+        String data = et.getText().toString();
+
+        if (data.isEmpty()){
+            Toast.makeText(this, "Please Enter Name", Toast.LENGTH_SHORT).show();
+        }else {
+            Intent i = new Intent(MainActivity.this, SecondActivity.class);
+            i.putExtra("mydata", data);
+            startActivity(i);
+        }
     }
 }
--- ExplicitIntent/app/src/main/java/com/example/explicitintent/SecondActivity.java
+++ ExplicitIntent/app/src/main/java/com/example/explicitintent/SecondActivity.java
@@ -3,6 +3,8 @@ package com.example.explicitintent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        String s = getIntent().getStringExtra("mydata");
+
+        TextView tv =findViewById(R.id.welcome);
+
+        tv.setText("Welcome : "+s);
+
     }
 }
