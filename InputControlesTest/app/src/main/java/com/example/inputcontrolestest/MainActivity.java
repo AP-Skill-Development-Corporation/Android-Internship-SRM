40bcd00c0bb0f57ae00dfbebc46a7d065bce48d7
Mastan Vali P <mastanvali.apssdc@gmail.com>
2020-07-03 11:48:34 +0530

    july03 content added

--- InputControlesTest/app/src/main/java/com/example/inputcontrolestest/MainActivity.java
+++ InputControlesTest/app/src/main/java/com/example/inputcontrolestest/MainActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     CheckBox ch_java,ch_android,ch_python;
     RadioButton r_male,r_female;
     String gender;
+    Spinner sp_district,sp_mandal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +35,48 @@ public class MainActivity extends AppCompatActivity {
         ch_java = findViewById(R.id.java);
         ch_android = findViewById(R.id.android);
         ch_python = findViewById(R.id.python);
+        sp_district = findViewById(R.id.district);
+        sp_mandal = findViewById(R.id.mandal);
         tv = findViewById(R.id.result);
+
+        ArrayAdapter<CharSequence> districts_adapter = ArrayAdapter.createFromResource(this,
+                R.array.districts, android.R.layout.simple_spinner_item);
+        sp_district.setAdapter(districts_adapter);
+
+
+        sp_district.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        Toast.makeText(MainActivity.this,
+                                "Please select Distict", Toast.LENGTH_SHORT).show();
+                    case 1:
+                        ArrayAdapter<CharSequence> atp_Mandals =
+                                ArrayAdapter.createFromResource(MainActivity.this,R.array.Anantapur_Mandals,
+                                        android.R.layout.simple_spinner_item);
+                        sp_mandal.setAdapter(atp_Mandals);
+                        break;
+                    case 2:
+                        ArrayAdapter<CharSequence> knl_Mandals =
+                                ArrayAdapter.createFromResource(MainActivity.this,R.array.Kurnool_Mandals,
+                                        android.R.layout.simple_spinner_item);
+                        sp_mandal.setAdapter(knl_Mandals);
+                        break;
+
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+
+
     }
 
     public void submit(View view) {
@@ -55,8 +101,11 @@ public class MainActivity extends AppCompatActivity {
             builder.append(ch_python.getText().toString());
         }
 
+        String selectedDistict = sp_district.getSelectedItem().toString();
+        String selectedMandal = sp_mandal.getSelectedItem().toString();
+
         tv.setText(name+"\n"+mobile+"\n"+email+"\n"+pass+"\n"
-                +gender+"\n"+builder);
+                +gender+"\n"+builder+"\n"+selectedDistict+"\n"+selectedMandal);
 
     }
 }
