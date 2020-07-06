cb9a9df9a5f8e465b212a4b9f989b90fe329d514
sai <saisankar75@gmail.com>
2020-07-06 23:59:32 +0530

    Pickers Updated

--- Menus&Pickers/app/src/main/java/com/example/menuspickers/MainActivity.java
+++ Menus&Pickers/app/src/main/java/com/example/menuspickers/MainActivity.java
@@ -4,28 +4,37 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.Toast;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
+
+    int c_date,c_month,c_year;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
         getMenuInflater().inflate(R.menu.menu,menu);
         return super.onCreateOptionsMenu(menu);
     }
+
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
@@ -47,9 +56,10 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-
         return super.onOptionsItemSelected(item);
     }
+
+
 
     public void alertDialog(View view) {
 
@@ -74,4 +84,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void dpd(View view) {
+
+        Calendar c = Calendar.getInstance();
+        c_year = c.get(Calendar.YEAR);
+        c_month = c.get(Calendar.MONTH);
+        c_date = c.get(Calendar.DATE);
+
+        DatePickerDialog pickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                Toast.makeText(MainActivity.this, dayOfMonth+"-"+(month+1)+"-"+year, Toast.LENGTH_SHORT).show();
+
+                /* Date - Month - Year*/
+            }
+        },c_year,c_month,c_date);
+
+        pickerDialog.show();
+
+    }
 }
