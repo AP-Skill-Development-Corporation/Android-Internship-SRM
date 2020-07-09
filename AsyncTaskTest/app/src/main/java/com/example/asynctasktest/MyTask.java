b37692438703ceb59974c99c695ea016be4e32da
Mastan Vali P <mastanvali.apssdc@gmail.com>
2020-07-09 14:42:46 +0530

    jul9-2020 notes added

--- AsyncTaskTest/app/src/main/java/com/example/asynctasktest/MyTask.java
+++ AsyncTaskTest/app/src/main/java/com/example/asynctasktest/MyTask.java
@@ -14,9 +14,7 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
-
 import javax.net.ssl.HttpsURLConnection;
 
 public class MyTask extends AsyncTask<Void,Void,String>
